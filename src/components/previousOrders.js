5e5463b8f9c42125e4ad4bb1511ea683aaddb609
Saurabhdimri06 <dimrisaurabh06@gmail.com>
2020-04-15 10:05:48 +0530

    Previous order page to check for previous orders

---
+++ src/components/previousOrders.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class PreviousOrders extends React.Component{
+
+    render(){
+        return(
+            <div className='orderDisplay'>
+                <div class="card" style={{width: '18rem'}}>
+                    <img class="card-img-top" src="..." alt="Food Item Image" />
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PreviousOrders
